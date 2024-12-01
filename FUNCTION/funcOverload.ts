f14720b5cf4266cba17a497fa747e18e786ed5f9
KARTHI-MAKES <karthiyamunarani5.com>
2024-12-01 12:41:30 +0530

    FUNCTIO OVERLOAD AND PRATICSE

---
+++ FUNCTION/funcOverload.ts
@@ -0,0 +1,9 @@
+export{}
+function add(a:number,b:number):number
+function add(a:string,b:string):string
+function add(a:any,b:any){
+      return a+b
+}
+console.log(add(2,3))
+console.log(add("karthick","A"))
+
